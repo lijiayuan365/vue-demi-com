af33f4658cbffb68f97d3eea431714ff07bd7bc0
lijiayuan <ljy@ljy,com>
2024-12-12 19:00:04 +0800

    feat: 使用babel 打包兼容es6

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import vue3 from 'rollup-plugin-vue3'
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
 
 console.log('=====rollup isVue2=====', isVue2)
 export default {
@@ -27,6 +28,11 @@ export default {
     isVue2 ? vue2() : vue3(),
     resolve(),
     commonjs(),
+    babel({
+      exclude: 'node_modules/**', // 排除 node_modules 目录
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'], // 支持的文件扩展名
+      babelHelpers: 'bundled' // 使用 Babel 自带的 helper 函数
+    }),
     // terser(),
   ]
 }
